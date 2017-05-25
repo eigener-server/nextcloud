fb9faf7bd70bbf2cbece1e868e469e80692f2998
www.eigener-server.ch <root@dev001.eigener-server.ch>
2017-05-25 15:57:26 +0200

    First Setup

---
+++ Dockerfile
@@ -0,0 +1,40 @@
+FROM eigenerserver/apache2:latest
+#FROM eigenerserver/apache2:0.1.0
+
+RUN apt-get update && \
+    apt-get -y --no-install-recommends install wget bzip2 && \
+    apt-get -y --no-install-recommends install --reinstall ca-certificates && \
+    apt-get clean && \
+rm -rf /var/lib/apt/lists/* /tmp/*
+
+ENV NEXTCLOUD_VERSION=12.0.0
+ENV NEXTCLOUD_GPG="2880 6A87 8AE4 23A2 8372 792E D758 99B9 A724 937A"
+
+RUN cd /tmp && \
+    NEXTCLOUD_DOWNLOAD="nextcloud-${NEXTCLOUD_VERSION}.tar.bz2" && \
+    wget -q https://download.nextcloud.com/server/releases/${NEXTCLOUD_DOWNLOAD} && \
+    wget -q https://download.nextcloud.com/server/releases/${NEXTCLOUD_DOWNLOAD}.sha256 && \
+    wget -q https://download.nextcloud.com/server/releases/${NEXTCLOUD_DOWNLOAD}.asc && \
+    wget -q https://nextcloud.com/nextcloud.asc && \ 
+    CHECK_1="$(echo -n $(sha256sum -c ${NEXTCLOUD_DOWNLOAD}.sha256) | tail -c 2)" && \
+    if [ "${CHECK_1}" != "OK" ]; then echo "Checksum match error: ${CHECK_1}" && exit 1; fi && \
+    gpg --import nextcloud.asc && \
+    CHECK_2="$(echo -n $(gpg --verify nextcloud-12.0.0.tar.bz2.asc nextcloud-12.0.0.tar.bz2 2>&1 \
+             | sed -n -e 's/^.*Primary key fingerprint: //p'  ))" && \
+    if [ "${CHECK_2}" != "${NEXTCLOUD_GPG}" ]; then echo "Fingerprint match error: ${CHECK_2}" && exit 1; fi && \
+
+    mkdir -p /var/www/html && \
+    tar xjvf ${NEXTCLOUD_DOWNLOAD} --strip 1 -C /var/www/html && \
+    rm -rf /tmp/*
+
+ENV NEXTCLOUD_DB_NAME=eigenerserver
+ENV NEXTCLOUD_DB_USER=eigenerserver
+ENV NEXTCLOUD_DB_PASSWORD=eigenerserver
+
+VOLUME /host/nextcloud/data /host/nextcloud/config /host/nextcloud/apps2
+
+COPY run.sh /usr/local/bin/run.sh
+RUN chmod +x /usr/local/bin/*
+
+ENTRYPOINT ["/bin/bash","/usr/local/bin/run.sh"]
+
---
+++ run.sh
@@ -0,0 +1,108 @@
+#!/bin/sh
+
+#set -e 
+
+if [ ! -f /firstrun ]; then
+
+    echo -e "\n" > /tmp/wait_for_mysql.php
+    sed -i -e '$a<?php' \
+           -e '$a\$connected = false;' \
+           -e '$awhile(!\$connected) {' \
+           -e '$a    try{' \
+           -e '$a        \$dbh = new pdo(' \
+           -e '$a            "mysql:host=mysql;dbname='${NEXTCLOUD_DB_NAME}'", "'${NEXTCLOUD_DB_USER}'", "'${NEXTCLOUD_DB_PASSWORD}'",' \
+           -e '$a            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)' \
+           -e '$a        );' \
+           -e '$a        \$connected = true;' \
+           -e '$a    }' \
+           -e '$a    catch(PDOException \$ex){' \
+           -e '$a        error_log("Could not connect to MySQL");' \
+           -e '$a        error_log(\$ex->getMessage());' \
+           -e '$a        error_log("Waiting for MySQL Connection.");' \
+           -e '$a        sleep(5);' \
+           -e '$a    }' \
+           -e '$a}' \
+    /tmp/wait_for_mysql.php
+
+    # Put the configuration and apps into volumes
+    ln -sf /host/nextcloud/config/config.php /var/www/html/config/config.php &>/dev/null
+    ln -sf /host/nextcloud/apps2 /var/www/html &>/dev/null
+
+    # Don't run this again
+    touch /firstrun
+fi
+
+/usr/local/bin/php /tmp/wait_for_mysql.php
+
+
+if [ ! -f /host/nextcloud/data/firstrun ]; then
+    # New installation, run the setup
+    mkdir -p /host/nextcloud/data/log
+    mkdir -p /host/nextcloud/apps2
+    mkdir -p /host/nextcloud/data
+    mkdir -p /host/nextcloud/config
+
+    chown -R www-data:www-data /var/www/html
+    chown -R www-data:www-data /host/nextcloud/apps2
+    chown -R www-data:www-data /host/nextcloud/data
+    chown www-data:www-data /host/nextcloud/config
+
+    CONFIGFILE=/host/nextcloud/config/config.php
+
+    echo -e "\n" >> $CONFIGFILE
+    instanceid=oc$(echo $PRIMARY_HOSTNAME | sha1sum | fold -w 10 | head -n 1)
+    sed -i -e '$a<?php' \
+           -e '$a\$CONFIG = array (' \
+           -e '$a  "datadirectory" => "/host/nextcloud/data",' \
+           -e '$a  "apps_paths" => array (' \
+           -e '$a      0 => array (' \
+           -e '$a              "path"     => "/var/www/html/apps",' \
+           -e '$a              "url"      => "/apps",' \
+           -e '$a              "writable" => false,' \
+           -e '$a      ),' \
+           -e '$a      1 => array (' \
+           -e '$a              "path"     => "/host/nextcloud/apps2",' \
+           -e '$a              "url"      => "/apps2",' \
+           -e '$a              "writable" => true,' \
+           -e '$a      ),' \
+           -e '$a  ),' \
+           -e '$a  "memcache.local" => "\OC\Memcache\Redis",' \
+           -e '$a  "filelocking.enabled" => "true",' \
+           -e '$a   "redis" => array(' \
+           -e '$a        "host" => "redis",' \
+           -e '$a        "port" => 6379,' \
+           -e '$a         ),' \
+           -e '$a  "instanceid" => "'${instanceid}'",' \
+           -e '$a  "trusted_domains" =>' \
+           -e '$a    array (' \
+           -e '$a      1 => "'${NEXTCLOUD_DOMAIN}'",' \
+           -e '$a    ),' \
+           -e '$a);' \
+           -e '$a?>' \
+        ${CONFIGFILE}
+
+    cd /var/www/html
+    # install db and admin user
+    /usr/local/bin/php occ maintenance:install --database "mysql" --database-name "${NEXTCLOUD_DB_NAME}" --database-user "$NEXTCLOUD_DB_USER" --database-pass "NEXTCLOUD_DB_PASSWORD" --admin-user "admin" --admin-pass "NEXTCLOUD_ADMIN_PASSWORD" --database-host "mysql"
+
+    # Don't run this again
+    touch /host/nextcloud/firstrun
+
+else
+    echo "auto upgrade disabled"
+    #occ upgrade
+    #if [ \( $? -ne 0 \) -a \( $? -ne 3 \) ]; then
+    #    echo "Trying ownCloud upgrade again to work around ownCloud upgrade bug..."
+    #    occ upgrade
+    #    if [ \( $? -ne 0 \) -a \( $? -ne 3 \) ]; then exit 1; fi
+    #    occ maintenance:mode --off
+    #    echo "...which seemed to work."
+    #fi
+fi
+
+chown -R www-data:www-data /var/www/html
+chown www-data:www-data /host/nextcloud/config/config.php
+chown -R www-data:www-data /host/nextcloud/apps2
+chown -R www-data:www-data /host/nextcloud/data
+
+exec "$@"
